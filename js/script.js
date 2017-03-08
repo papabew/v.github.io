688efef8d9db4eb745d2a43e5a811353ac9cf2ff
DESKTOP-QUN5BO8\bew <johnvincentbernabe@gmail.com>
2017-03-09 04:10:21 +0800

    My first personal website.

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+	//small devices navigation toggle
+	var menu = $("#navigation");
+	$("#navBtn").click(function(){
+		menu.slideToggle();
+	});
+
+	//window resize navigation show
+	$(window).resize(function(){
+		var width = $(document).width();
+		if (width >= 768 && menu.is(':hidden')) {
+			menu.css('display', '');
+		}
+	});
+
+	var doc = $(document),
+		headerBg = $(".header-bg");
+	var home = $("#home").offset().top;
+	var about = $("#about").offset().top;
+	var skills = $("#skills").offset().top;
+	var expi = $("#experience").offset().top;
+	var contact = $("#contact").offset().top;
+
+	doc.scroll(function(){
+		var currScrollPos = doc.scrollTop();
+		headerBg.css('background-position', '50% ' + -currScrollPos/4 + 'px');
+		var navLink = $("a[href^='#']");
+		if (currScrollPos >= about && currScrollPos <= about+20) {
+			navLink.removeClass("active");
+		}
+	});
+
+	console.log(doc.scrollTop());
+
+	var navHeight = $("nav").outerHeight();
+
+	$("a[href^='#']").click(function(e){
+		var page = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(page).offset().top - navHeight
+		}, 500, "linear");
+
+		$(".link").removeClass("active");
+		$(this).addClass("active");
+
+		e.preventDefault();
+	});
+
+
+
+	$('input, textarea').blur(function(){
+
+		console.log($(this).val());
+
+		if ($(this).val())
+			$(this).addClass("used");
+		else 
+			$(this).removeClass("used");
+	});
+
+
+
+});
