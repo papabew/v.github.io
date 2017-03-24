c66c7ae287583db3def8a8fa241fc60d1f91bcf8
DESKTOP-QUN5BO8\bew <johnvincentbernabe@gmail.com>
2017-03-24 20:44:46 +0800

    fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	$("#overlay").delay(750).fadeOut(200);
     window.scrollTo(0,0);
 
 	//small devices navigation toggle
@@ -99,6 +98,5 @@ $(document).ready(function() {
     });
 
 
-
-
+	$("#overlay").fadeOut(200);
 });
